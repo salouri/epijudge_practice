3d821b2eec29c6f47e4c22f31c65e6024ae6f5fe
Saeed Badran <salouri@gmail.com>
2023-09-21 08:25:13 +0300

    Task: count bits in positive integers

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -3,10 +3,14 @@ from test_framework import generic_test
 
 def count_bits(x: int) -> int:
     # TODO - you fill in here.
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x = x >> 1
+    return num_bits
     return 0
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+print(count_bits(6))
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("count_bits.py", "count_bits.tsv", count_bits))
